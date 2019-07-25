ba8c2cc16689c3eca0f660b56413e41904650cdf
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-07-25 14:25:11 +0300

    task3-comments

--- src/app/core/services/config-options.service.ts
+++ src/app/core/services/config-options.service.ts
@@ -3,17 +3,16 @@ import { ConfigOptionsModel } from '../models/config-options.model';
 import { CoreModule } from '../core.module';
 
 @Injectable({ providedIn: CoreModule })
-
 export class ConfigOptionsService {
   private option: ConfigOptionsModel;
 
-  constructor() { }
+  constructor() {}
 
   getOption(): ConfigOptionsModel {
     return this.option;
   }
 
   setOption(option: ConfigOptionsModel): void {
-    this.option = option;
+    this.option = { ...this.option, ...option };
   }
 }
